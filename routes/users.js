c0042e1452ac145f8073cf10e4e7c2d10d40d443
Luis Arce <luis.arce22@gmail.com>
2019-04-30 16:13:21 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const usersList = [
+  {
+    id: 1,
+    name: 'admin',
+    login: 'admin',
+    password: 'admin'
+  },
+  {
+    id: 2,
+    name: 'guest',
+    login: 'guest',
+    password: 'guest'
+  }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(usersList);
 });
 
 module.exports = router;
