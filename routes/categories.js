cc128a81ec2a2c6817cc85df6e24d66531f89e36
Luis Arce <luis.arce22@gmail.com>
2019-05-09 22:34:00 -0400

    Adding price and quantity and description to products

--- routes/categories.js
+++ routes/categories.js
@@ -10,17 +10,26 @@ let categories = [
           {
             id: 1,
             name: "Banana",
-            img: "https://target.scene7.com/is/image/Target/GUEST_f5d0cfc3-9d02-4ee0-a6c6-ed5dc09971d1?wid=488&hei=488&fmt=pjpeg"
+            price: 10,
+            quantity: 10,
+            img: "https://target.scene7.com/is/image/Target/GUEST_f5d0cfc3-9d02-4ee0-a6c6-ed5dc09971d1?wid=488&hei=488&fmt=pjpeg",
+            description: "A banana is a curved, yellow fruit with a thick skin and soft sweet flesh. ... It grows in bunches on a banana tree. To 'go bananas' is a slangy way of saying 'go crazy' or act ridiculous, and if someone refers to the 'top banana,' they mean the most important person in a particular group."
           },
           {
             id: 2,
             name: "PineApple",
-            img: "https://www.organicfacts.net/wp-content/uploads/pineapplecalories.jpg"
+            price: 20,
+            quantity: 50,
+            img: "https://www.organicfacts.net/wp-content/uploads/pineapplecalories.jpg",
+            description: "Pineapple, Ananas comosus, is an herbaceous biennial or perennial plant in the family Bromeliaceae grown for its edible fruit. The pineapple plant has a short stout stem and a rosette of sword-shaped leaves with needle-like tips. ... Pineapple originates from the tropical regions of the Americas."
           },
           {
             id: 3,
             name: "Orange",
-            img: "http://soappotions.com/wp-content/uploads/2017/10/orange.jpg"
+            price: 15,
+            quantity: 100,
+            img: "http://soappotions.com/wp-content/uploads/2017/10/orange.jpg",
+            description: "The orange is the fruit of the citrus species Citrus × sinensis in the family Rutaceae. ... Orange trees are widely grown in tropical and subtropical climates for their sweet fruit. The fruit of the orange tree can be eaten fresh, or processed for its juice or fragrant peel."
           },
 
         ]
